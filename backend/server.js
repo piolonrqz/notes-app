77d54cc574a11946c466dea42bfb29656c2c1a5d
piolonrqz <piolofrances.enriquez@cit.edu>
2025-09-23 15:04:39 +0800

    Setup of frontend react-vite + tailwind css and backend express + mongo

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express backend!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+  })
+  .catch((err) => console.error(err));
