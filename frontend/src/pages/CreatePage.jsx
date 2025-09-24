178e1ab92ea96824d006fef8c4e4c3f0202e3db8
piolonrqz <piolofrances.enriquez@cit.edu>
2025-09-24 14:53:02 +0800

    finalized and completed the notes app

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,90 +1,88 @@
-import { ArrowLeftIcon } from 'lucide-react';
-import React, { useState } from 'react';
-import { Link, useNavigate } from "react-router";
-import axios from "axios";
-import toast from "react-hot-toast";
-import api from '../lib/axios';
+import { ArrowLeftIcon } from 'lucide-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
+import api from '../lib/axios'
+import NavigationBar from '../components/NavigationBar'
+
 const CreatePage = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    if(!title.trim() || !content.trim()){
-      toast.error("All fields are required");
-      return;
+    e.preventDefault()
+    if (!title.trim() || !content.trim()) {
+      toast.error('All fields are required')
+      return
     }
 
     setLoading(true)
 
     try {
-      await api.post("/notes", {
-        title,
-        content,
-      });
-      toast.success("Note created successfully");
-      navigate("/")
+      await api.post('/notes', { title, content })
+      toast.success('Note created successfully')
+      navigate('/')
     } catch (error) {
-        console.log("Error in note creation", error);
-        if(error.response.status === 429){
-          toast.error("You're creating notes too fast",{ 
-            duration: 4000,
-            icon: "🤬"
-        }); 
-        } else {
-          toast.error("Failed to create a note");
-        }
+      console.log('Error in note creation', error)
+      if (error.response?.status === 429) {
+        toast.error("You're creating notes too fast", {
+          duration: 4000,
+          icon: '🤬',
+        })
+      } else {
+        toast.error('Failed to create a note')
+      }
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-
   }
-  return (
-    <div className='min-h-screen bg-base-200'>
-      <div className='container px-4 py-8 mx-auto'>
-        <div className='max-w-2xl mx-auto'>
-          <Link to={"/"} className='mb-6 btn btn-ghost'>
-          <ArrowLeftIcon className='size-5'/>
-          Back to Notes
-          </Link>
 
-          <div className='card bg-base-100'>
-            <div className='card-body'>
-              <h2 className='mb-4 ml-2 text-2xl card-title'> Create New Note </h2>
+  return (
+    <div className="min-h-screen bg-base-200">
+      <NavigationBar />
+      <div className="container px-4 py-8 mx-auto">
+        <div className="max-w-lg mx-auto">
+          <Link to={'/'} className="inline-flex items-center gap-2 mb-6 btn btn-ghost">
+            <ArrowLeftIcon className="w-5 h-5" />
+            Back to Notes
+          </Link>
+          <div className="shadow-sm card card-compact bg-base-100">
+            <div className="p-4">
+              <h2 className="mb-3 text-xl font-semibold">Create New Note</h2>
               <form onSubmit={handleSubmit}>
-                <div className='mb-4 form-control'>
-                  <label className='label'>
-                    <span className='ml-2 label-text'>Title</span>
+                <div className="mb-3 form-control">
+                  <label className="label">
+                    <span className="label-text">Title</span>
                   </label>
-                  <input type='text' 
-                  placeholder='Note title'  
-                  className='input input-bordered'
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}/>
+                  <input
+                    type="text"
+                    placeholder="Note title"
+                    className="w-full input input-bordered"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
                 </div>
 
-                <div className='mb-4 form-control'>
-                  <label className='label'>
-                      <span className='ml-2 label-text'>
-                        Content
-                      </span>
+                <div className="mb-3 form-control">
+                  <label className="label">
+                    <span className="label-text">Content</span>
                   </label>
 
-                  <textarea 
-                    placeholder='Write your note here'
-                    className='h-32 mb-4 textarea textarea-bordered'
+                  <textarea
+                    placeholder="Write your note here"
+                    className="w-full mb-3 h-28 textarea textarea-bordered"
                     value={content}
                     onChange={(e) => setContent(e.target.value)}
-                    />
-                <div className='justify-end card-actions '>
-                  <button type="submit" className='btn btn-primary' disabled={loading}>
-                    {loading ? "Creating..." : "Create Note"}
-                  </button>
+                  />
+                  <div className="justify-end card-actions ">
+                    <button type="submit" className="btn btn-primary" disabled={loading}>
+                      {loading ? 'Creating...' : 'Create Note'}
+                    </button>
+                  </div>
                 </div>
-                </div>
-                </form>
+              </form>
             </div>
           </div>
         </div>
